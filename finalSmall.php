ce44c48453a1b8e7174cc83ecd8bc34a5a626927
AlenaBekhtereva <71527780+AlenaBekhtereva@users.noreply.github.com>
2020-11-24 15:34:40 +0300

    Add files via upload

---
+++ finalSmall.php
@@ -0,0 +1,14 @@
+<html
+<head>
+</head>
+<body>
+Спасибо за участие в опросе!
+<?php
+$comment = $_POST['comment'];
+$kind = $_POST['kind'];
+$teacher = $_POST['teacher'];
+echo $comment;
+
+?>
+</body>
+</html>
