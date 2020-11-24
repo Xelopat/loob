da521b0e46d53b6dc67ca30c75d845e8bb17940a
AlenaBekhtereva <71527780+AlenaBekhtereva@users.noreply.github.com>
2020-11-24 16:42:59 +0300

    Update finalBig.php

--- finalBig.php
+++ finalBig.php
@@ -8,6 +8,12 @@ $teacher = $_POST['teacher'];
 $sum=0;
 for($i=1; $i < 16; $i++){
 	$sum+=$_POST[$i];
+}for($i=1; $i < 16; $i++){
+	if ($_POST[$i] == null){
+	$amount--;
+	} else {
+	$sum+=$_POST[$i];
+	}
 }
 ?>
 </body>
