df3eb489d881fe42eba88076c8e5402c5ed35278
Xelopat <61018159+Xelopat@users.noreply.github.com>
2020-11-24 16:02:15 +0300

    Add files via upload

--- base.php
+++ base.php
@@ -36,6 +36,19 @@
 		}
 	}
 	
+	function get_comments_text($name){
+		global $link;
+		$sql = "SELECT * FROM teachers WHERE teacher = '$name'";
+		$result = $link->query($sql);
+
+		if ($result->num_rows > 0) {
+		  // output data of each row
+		  while($row = $result->fetch_assoc()) {
+			return $row["comments"];
+		  }
+		}
+	}
+	
 	function get_all(){
 		global $link;
 		
@@ -127,9 +140,9 @@
 	
 	function add_comment($comment, $positive, $name){
 		global $link;
-		$plus = "0";
-		if ($positive) $plus = "1";
-		$comments = get_comments($name);
+		$plus = "1";
+		if ($positive) $plus = "5";
+		$comments = get_comments_text($name);
 		$sum_comment = $comments . "^^^" . $plus . $comment ;
 		$result = "UPDATE teachers SET comments='$sum_comment' WHERE teacher='$name'";
 		
