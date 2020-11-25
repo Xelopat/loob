f00d8811b8015116505870b3c02ea4de4d2f2407
AlenaBekhtereva <71527780+AlenaBekhtereva@users.noreply.github.com>
2020-11-25 11:22:03 +0300

    Add files via upload

---
+++ add_teacher.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+</head>
+<body>
+<form name=addTeacher action=final_teacher.php method=POST>
+
+<input type=text name=nameTeacher> <br/>
+<input type="submit" name=addTeacher value="Добавить учителя" />
+</form>
+</body>
+</html>
