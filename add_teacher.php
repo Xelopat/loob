3f389d31e3075cb1353fc97922041459bf5ddcee
AlenaBekhtereva <71527780+AlenaBekhtereva@users.noreply.github.com>
2020-11-25 14:10:45 +0300

    Update add_teacher.php

--- add_teacher.php
+++ add_teacher.php
@@ -2,9 +2,19 @@
 <head>
 </head>
 <body>
+Введите имя учителяи предмет, который он ведет <br/>
 <form name=addTeacher action=final_teacher.php method=POST>
 
 <input type=text name=nameTeacher> <br/>
+<select name=subject>
+<option name=math>Математика</option>
+<option name=history>История</option>
+<option name=english>Английский язык</option>
+<option name=russian>Русский язык</option>
+<option name=literature>Литература</option>
+<option name=physics>Физика</option>
+<option name=chemistry>Химия</option>
+</select><br/>
 <input type="submit" name=addTeacher value="Добавить учителя" />
 </form>
 </body>
